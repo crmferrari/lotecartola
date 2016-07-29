1b4b16b6ba82e8ac57fbf67c984a75e3348c9eee
Christiano <crmatos@gmail.com>
2016-07-29 08:45:11 -0300

    Funcionando sem erro.

--- lotecartola/src/com/lotecartola/entity/Usuario.java
+++ lotecartola/src/com/lotecartola/entity/Usuario.java
@@ -1,11 +1,50 @@
 package com.lotecartola.entity;
 
 import java.io.Serializable;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "USUARIO")
 public class Usuario implements Serializable {
 
+	@Id
+	@Column(name = "id_usuario", nullable = false)
+	private Integer idUsuario;
+
+	public Integer getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Integer idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((idUsuario == null) ? 0 : idUsuario.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (idUsuario == null) {
+			if (other.idUsuario != null)
+				return false;
+		} else if (!idUsuario.equals(other.idUsuario))
+			return false;
+		return true;
+	}
 }
